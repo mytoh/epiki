c9498537c50538d5716f425ceae6a9066611adb8
mytoh <mytoh@gmail.com>
2016-02-13 03:13:12 +0900

    * lisp/ekuva.el: refactor

--- lisp/ekuva.el
+++ lisp/ekuva.el
@@ -17,46 +17,27 @@
 (cl-defun ekuva:call-process (com &rest args)
   (apply #'call-process com nil nil nil args))
 
-(cl-defun ekuva:feh-open-file (file)
-  (cl-letf ((dir (file-name-directory file)))
-    (ekuva:call-process
-     "feh" "-Z" "-F" "-B black" dir)))
-
-(cl-defun ekuva:feh-open-directory (dir)
+(cl-defun ekuva:feh-open (dir)
   (ekuva:call-process
    "feh" "-Z" "-F" "-B black" dir))
 
-(cl-defun ekuva:sxiv-open-file (file)
-  (cl-letf ((dir (thread-first file
-                   file-name-directory
-                   ekuva:remove-trailing-slash)))
-    (ekuva:call-process "sxiv"
-                        "-f" "-s" "f" "-b"  "-r" dir)))
+(cl-defun ekuva:sxiv-open (dir)
+  (ekuva:call-process "sxiv" "-f" "-s" "f" "-b" "-r" dir))
 
-(cl-defun ekuva:sxiv-open-directory (dir)
-  (ekuva:call-process "sxiv" "-f" "-s" "f" "-b" "-r" 
-                      (thread-first dir ekuva:remove-trailing-slash)))
-
-(cl-defun ekuva:open-function-file ()
+(cl-defun ekuva:open-function ()
   (pcase t
     ((guard (executable-find "sxiv"))
-     #'ekuva:sxiv-open-file)
+     #'ekuva:sxiv-open)
     ((guard (executable-find "feh"))
-     #'ekuva:feh-open-file)))
-
-(cl-defun ekuva:open-function-directory ()
-  (pcase t
-    ((guard (executable-find "sxiv"))
-     #'ekuva:sxiv-open-directory)
-    ((guard (executable-find "feh"))
-     #'ekuva:feh-open-directory)))
+     #'ekuva:feh-open)))
 
 (cl-defun ekuva:open (file)
   (pcase file
     ((pred file-regular-p)
-     (funcall (ekuva:open-function-file) file))
+     (funcall (ekuva:open-function)
+        (file-name-directory file)))
     ((pred file-directory-p)
-     (funcall (ekuva:open-function-directory) file))))
+     (funcall (ekuva:open-function) file))))
 
 (cl-defun ekuva (args)
   (ekuva:open (expand-file-name args))
