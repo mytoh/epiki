49235f40186ca8009c042cdd05181ee6fbac4587
mytoh <mytoh@gmail.com>
2016-05-27 09:02:24 +0900

    * lisp/ecfn.el: add

---
+++ lisp/ecfn.el
@@ -0,0 +1,13 @@
+;;; ecfn -- ecfn -*- lexical-binding: t; coding: utf-8; -*-
+
+;;; Commentary:
+
+;;;; clean up file names
+;;;;;  remove white space
+;;;;;  replace dash to underscore
+
+;;; Code:
+
+
+
+;;; ecfn.el ends here
